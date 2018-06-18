3e93960694dddce75e407e896bd6f2ef4cf6bfc7
zizou <choizidane@naver.com>
2018-06-18 11:23:19 +0900

    init project

---
+++ src/main/java/com/inc/day/DayApplication.java
@@ -0,0 +1,12 @@
+package com.inc.day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayApplication.class, args);
+	}
+}
