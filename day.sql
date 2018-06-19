7f3ceb66725dd5f98cb236b6a4357796b8077c1d
zizou <choizidane@naver.com>
2018-06-19 11:33:34 +0900

    데이터베이스 주소 수정

--- day.sql
+++ day.sql
@@ -14,3 +14,4 @@ create sequence seq_day_id;
 
 insert into day values(seq_day_id.nextval, 'gogogo', '첫번째 글', '곧 점심시간이다', sysdate);
 insert into day values(seq_day_id.nextval, 'kkkkkk', '두번째 글', '식빵', sysdate);
+
